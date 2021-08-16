f0dcd8b8a0aacaa7992f234c102869bf3f1663c2
FergusKidd <45766226+FergusKidd@users.noreply.github.com>
2021-08-16 13:20:38 +0100

    feat(webapp-backend): adds layout results function

--- clean-room-demo/webapp/backend/formrecognizer.py
+++ clean-room-demo/webapp/backend/formrecognizer.py
@@ -11,11 +11,19 @@ def get_layout_url():
 
 def test_layout_connection():
     """returns true if the contianer is alive, false if not"""
-    print(get_layout_url())
-
-    r = requests.get("http://" + get_layout_url() + "/ContainerLiveness")
+    url = get_layout_url()
+    r = requests.get(f("http://{url}/ContainerLiveness"))
 
     if r.status_code == 200:
         return True
     else:
         return False
+
+
+def layout_results(op_Id):
+    url = get_layout_url()
+    r = requests.get(
+        (f"http://{url}/formrecognizer/v2.1/layout/analyzeResults/{op_Id}")
+    )
+
+    return r.content
