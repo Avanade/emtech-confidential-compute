b90adda7f4236076dc526bda6b0a302f8efd0a1f
FergusKidd <45766226+FergusKidd@users.noreply.github.com>
2021-08-27 14:20:47 +0100

    feat(webapp-backend): simplifies status return for user functions

--- clean-room-demo/webapp/backend/users/users.py
+++ clean-room-demo/webapp/backend/users/users.py
@@ -5,12 +5,24 @@ sys.path.append("clean-room-demo/webapp/backend/sql")
 import sql.sqlinteractions as si
 
 
+def status_return(status):
+    if status == True:
+        return {"verification status": "True"}
+    else:
+        return {"error": "user is not validated"}
+
+
 def verify(id):
     """verify a user id exists in the system"""
 
     status = si.check_access(id)
 
-    if status == True:
-        return {"verification status": "True"}
-    else:
-        return {"error": "user is not validated"}
+    return status_return(status)
+
+
+def verify_doc_access(user_id, doc_id):
+    """verify user has access to a specific document"""
+
+    status = si.check_doc_access(user_id, doc_id)
+
+    return status_return(status)
