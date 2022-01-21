f2e89ffab358beeb577f4461225060978d38357a
Rainier Mendiola <rainier.mendiola@accenture.com>
2022-01-21 15:02:23 +0800

    updated Loading

--- clean-room-demo/webapp/frontend/components/Loading.tsx
+++ clean-room-demo/webapp/frontend/components/Loading.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next"
 
 const Loading:NextPage = () => {
     return (
-        <div className="animate-spin text-center">
+        <div className="flex animate-spin place-content-center">
             {/* w-16 h-16 border-4 border-blue-400 border-dashed rounded-full */}
             <svg width="75" height="75" viewBox="0 0 75 75" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <circle cx="37.5009" cy="6.79995" r="6.01089" fill="#0759A7"/>
