82438f3c9fe3b886c4d8256eb010fbeea18cd96f
dennisdelamida <dennis.delamida@accenture.com>
2022-01-21 20:11:42 +0800

    updage message

--- clean-room-demo/webapp/frontend/components/Message.tsx
+++ clean-room-demo/webapp/frontend/components/Message.tsx
@@ -1,36 +1,32 @@
 import { NextPage } from "next"
 import Loading from './Loading'
-import {CheckCircleIcon,XCircleIcon,InformationCircleIcon} from '@heroicons/react/outline'
+import { CheckCircleIcon, XCircleIcon, InformationCircleIcon } from '@heroicons/react/outline'
 import { Switch } from "@headlessui/react"
 
-const Message: NextPage<{ message1: string, message2: string,type:'info'|'success'|'loading'|'error' }> = (context) => {
-    const { message1, message2  } = context
-    let returnmsg:any
-        switch (context.type) {
-            case    'info':
-                            returnmsg= <InformationCircleIcon  height={75} width={75} color="Red"  />
-                            break;
-            case 'success':
-                            returnmsg=<CheckCircleIcon height={75} width={75} color="Green"/>  
-                            break;
-            case 'loading':
-                            returnmsg= <Loading />
-                            break;
-            default:
-                            returnmsg=<XCircleIcon  height={75} width={75} color="Red"  />
-                            break;
-        }
+const Message: NextPage<{ message1: string, message2: string, type: 'info' | 'success' | 'loading' | 'error' }> = (context) => {
+    const { message1, message2 } = context
+    let returnmsg: any
+    switch (context.type) {
+        case 'info':
+            returnmsg = <InformationCircleIcon height={75} width={75} color="blue" />
+            break;
+        case 'success':
+            returnmsg = <CheckCircleIcon height={75} width={75} color="Green" />
+            break;
+        case 'loading':
+            returnmsg = <Loading />
+            break;
+        default:
+            returnmsg = <XCircleIcon height={75} width={75} color="Red" />
+            break;
+    }
     return (
         <>
-        <div className="flex flex-col h-full place-items-center text-center">
-            <div className="basis-2/4 h-2/4">
-                  
-                         { returnmsg}
-                   
-             </div>
-            <div className="text-blue-800 basis-1/4 h-1/4 text-lg">{message1}</div>
-            <div className="text-sm  basis-1/4"   >{message2}</div>
-        </div>
+            <div className="flex flex-col h-full place-items-center text-center">
+                <div className="basis-2/4 h-2/4">{returnmsg}</div>
+                <div className="text-blue-800 basis-1/4 h-1/4 text-lg">{message1}</div>
+                <div className="text-sm  basis-1/4">{message2}</div>
+            </div>
         </>
     )
 }
