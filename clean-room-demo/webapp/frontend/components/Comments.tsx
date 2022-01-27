3ff83b5249a918b75d9f92514d3d3ddcea57c3d3
Rainier Mendiola <rainier.mendiola@accenture.com>
2022-01-27 15:59:47 +0800

    View existing comments Fixes #78

--- clean-room-demo/webapp/frontend/components/Comments.tsx
+++ clean-room-demo/webapp/frontend/components/Comments.tsx
@@ -3,30 +3,8 @@ import Button from "./Button";
 import CommentItem, {IComment} from './CommentItem'
 
 
-// sample data
-let d = new Date('01/25/2022')
-let comments:IComment[] = [
-    {
-        id: 1,
-        name: 'Rainier',
-        date: d,
-        body: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
-    },
-    {
-        id: 2,
-        name: 'Dennis',
-        date: d,
-        body: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
-    },
-    {
-        id: 3,
-        name: 'Jerrico',
-        date: d,
-        body: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
-    },
-]
-
-const Comments:NextPage = () => {
+const Comments:NextPage<{comments?:IComment[]}> = (context) => {
+    const {comments} = context
     const addReplyButton = (id) => {
         console.log(`Add reply is clicked on item id ${id}`)
     }
