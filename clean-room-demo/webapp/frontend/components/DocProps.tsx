8837c78ccd9386f8ec3ad734bd8b491feb1af56c
Rainier Mendiola <rainier.mendiola@accenture.com>
2022-01-28 22:39:44 +0800

    updated updating of tags and description

--- clean-room-demo/webapp/frontend/components/DocProps.tsx
+++ clean-room-demo/webapp/frontend/components/DocProps.tsx
@@ -1,5 +1,5 @@
 import { IDocument } from "@/lib/interface/document";
-import { PencilIcon } from "@heroicons/react/outline";
+import { PencilIcon, PlusIcon, HashtagIcon } from "@heroicons/react/outline";
 import { NextPage } from "next";
 import { useState } from "react";
 import Avatar from "./Avatar";
@@ -7,6 +7,7 @@ import Button from "./Button";
 import KeyValue from "./KeyValue";
 import Modal from "./Modal";
 import PopupButton from "./PopupButton";
+import Textbox from "./Textbox";
 
 
 const DocProps:NextPage<{document:IDocument}> = (context) => {
@@ -15,9 +16,28 @@ const DocProps:NextPage<{document:IDocument}> = (context) => {
     const [modalTags, setModalTags] = useState(false)
     const [modalShared, setModalShared] = useState(false)
     const [modalDescription, setModalDescription] = useState(false)
+    const [newTag, setNewTag] = useState("")
+    const [newTagExists, setNewTagExists] = useState(false)
 
     const updateDescription = (v) => {
-        setData(prev => ({...prev,description:v}))
+        setData(prev => ({...prev, description:v}))
+    }
+
+    const removeTag = (n) => {
+        setData(prev => ({...prev, tags:data.tags.filter(v=>v!=n)}))
+    }
+
+    const addTag = () => {
+        if(!data.tags.map(v=>{return v.toLowerCase()}).includes(newTag.toLowerCase())) {
+            let tag = data.tags
+            tag.push(newTag)
+            // console.log('adding tag: ' + newTag)
+            setData(prev => ({...prev, tags:tag}))
+            setNewTag('')
+        } else {
+            console.log('existing')
+            setNewTagExists(true)
+        }
     }
 
     return (
@@ -51,7 +71,7 @@ const DocProps:NextPage<{document:IDocument}> = (context) => {
             <div className="m-3 my-5">
                 <KeyValue name="Shared With" vertical value={
                 data.sharedWith.map((n,i) => (
-                    <Avatar letter={n.name.substring(0,1)} className="mr-1"></Avatar>  
+                    <Avatar key={i} letter={n.name.substring(0,1)} className="mr-1"></Avatar>  
                 ))
                 }
                 action={<>
@@ -67,14 +87,26 @@ const DocProps:NextPage<{document:IDocument}> = (context) => {
             <div className="m-3 my-5">
                 <KeyValue name="Tags" vertical value={
                 data.tags.map((t,i)=>(
-                    <Button small className="mr-1 bg-amber-400">{t}</Button>
+                    <span key={i}>
+                        <Button small className="bg-amber-400">{t}</Button>
+                        <Button small textLayout className="bg-amber-400 mr-1 px-1 hover:bg-cyan-800" onClick={()=>removeTag(t)}>X</Button>
+                    </span>
                 ))
                 }
                 action={<>
-                    <Button textLayout onClick={()=>setModalTags(true)}><PencilIcon width={15} className="text-primary"/></Button>
-                    {/* <PopupButton noBackground popupPosition="left" label={<PencilIcon width={15} className="text-primary"/>}></PopupButton> */}
-                    <Modal title="Tags" visible={modalTags} onClose={()=>setModalTags(false)}>
-                        Tags
+                    {/* <PopupButton noBackground popupPosition="left" label={<PlusIcon width={15} className="text-primary"/>} className=""></PopupButton> */}
+                    <Button textLayout onClick={()=>setModalTags(true)}><PlusIcon width={15} className="text-primary"/></Button>
+                    <Modal title="Add Tag" visible={modalTags} onClose={()=>setModalTags(false)}>
+                        <div className="flex flex-col">
+                            <div>
+                                <Textbox id="txtTag" name="Enter Tag" invalid={newTagExists} value={newTag} type="text" onChange={(v:string)=>{setNewTag(v); setNewTagExists(false)}} preicon={HashtagIcon}/>
+                            </div>
+                            {/* Line below can be removed. Use invalidMessage property on Textbox when available. */}
+                            {newTagExists ? <div className="text-xs text-red-500 py-1"> *This tag is already existing.</div> : null}
+                            <div className="self-center">
+                                <Button onClick={()=>addTag()}>Save</Button>
+                            </div>
+                        </div>
                     </Modal>
                     </>}
                 ></KeyValue>
